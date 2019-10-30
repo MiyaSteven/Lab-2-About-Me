5cdb313c9efc0465907fddc7cc8e3a4a5c5cccbd
swm92 <swm9220@gmail.com>
2019-10-30 16:29:40 -0700

    Lab-3 partial

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var userName = prompt('What is your name?')
-    alert('Welcome ' + userName + '! Enjoy the first page to my Life!');
+    alert('Welcome ' + userName + '! Enjoy the first page to my website!');
 
 var answer1 = confirm('Do you like food?')
     if (answer1 === true) {
@@ -28,7 +28,7 @@ var answer3 = confirm('Will you try to complete all the labs?')
         };
 
 var answer4 = confirm('Do you like to reach your goals?')
-    if (answer4 === true){
+    if (answer4 === true) {
         alert('Correct!')
         // console.log('Awesome you are!')
         } else {        
@@ -36,13 +36,40 @@ var answer4 = confirm('Do you like to reach your goals?')
         // console.log('Better start smaller')
         };
 
-var answer5 = confirm('Do you love yourself?')
-    if (answer5 === true){
+var answer5 = confirm('Do you love coding?')
+    if (answer5 === true) {
         alert('Correct!')
-        // console.log('Great, we are ready to begin...')
+        // console.log('Great, we are ready to begin.')
         } else {
         alert('Incorrect!')
-        // console.log('It is okay because we love you')
+        // console.log('It is okay because we love coding')
         };
 
-alert('Enjoy the rest of my novel, ' + userName + '.');
+var question6 = prompt('Pick a number between 1 and 4. You have 4 guesses!') 
+//how to add numeric input
+var answer6 = 4;
+    for (var i=0; i < 4; i++)
+//how to combine answer6 and i together to give 4 guesses
+        //  console.log(i);
+    if (answer6 === 4) {
+        alert('Correct!')
+        //  console.log('Awesome! Moving forward!')
+    } else { 
+        while (answer6  !== 4)
+        alert('too low')
+        //  console.log(loop until 3 incorrect guesses)
+    };
+//add correct answer after guesses exhausted + show answer
+
+var questionSeven = prompt('What countries do you think my favorite foods are from? You have 6 guesses.');
+var array = ['USA', 'Japan', 'China', 'Italy', 'France', 'Korea', 'Greece', 'Thailand'];
+if (var i = 0; i < 6; i++) {
+    alert('Correct!')
+    } else (var i > 5) {
+    alert('The correct answers were: ' + array)
+    };
+
+var correctAnswers = [];
+//how to sum up correct answers
+
+alert('Enjoy the rest of my website, ' + userName + '.');
